4617585fcb82d3e641a236dcbb52d48635dbf440
raynerljm <raynerljm@gmail.com>
2023-01-14 14:02:06 +0800

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rtt, setRtt] = useState("");
+  const [downlink, setDownlink] = useState("");
+  const [count, setCount] = useState(0);
+
+  const loop = () => {
+    setTimeout(() => {
+      const x = window.navigator.connection;
+      setCount(count + 1);
+      setRtt(x.rtt);
+      setDownlink(x.downlink);
+
+      loop();
+    }, 500);
+  };
+
+  loop();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>RTT: {rtt}</h1>
+      <h1>Downlink: {downlink}</h1>
+      <h1>Times pinged: {count}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
